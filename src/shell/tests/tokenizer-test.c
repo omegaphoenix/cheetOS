8984e78484132646443000afb9bf1d63eadc5319
Justin Leong <justinleong16@gmail.com>
2017-01-10 14:14:01 -0800

    Change indentation to 4 spaces
    Using indentation style which matches test files provided
    by Donnie in the assignment.

--- src/shell/tests/tokenizer-test.c
+++ src/shell/tests/tokenizer-test.c
@@ -6,73 +6,73 @@
 
 /* Test basic 3 word command */
 void parse_basicCommand_splitByWhitespace() {
-  char *test_str = "a arg1 arg2";
+    char *test_str = "a arg1 arg2";
 
-  /* Expected results */
-  int num_words = 3;
-  char *expected[] = {"a", "arg1", "arg2"};
-  token_type expected_tokens[] = {WORD, WORD, WORD};
+    /* Expected results */
+    int num_words = 3;
+    char *expected[] = {"a", "arg1", "arg2"};
+    token_type expected_tokens[] = {WORD, WORD, WORD};
 
-  char **words = malloc(KiB(1));
-  token_type *tokens = malloc(KiB(1) * sizeof(token_type));
+    char **words = malloc(KiB(1));
+    token_type *tokens = malloc(KiB(1) * sizeof(token_type));
 
-  /* Call parse function to be tested */
-  int res = parse_tokens(test_str, words, tokens);
-  if (res != num_words) {
-    fprintf(stderr, "Wrong number of tokens\n");
-  }
-
-  /* Check for correct split of words */
-  for (int i = 0; i < num_words; i++) {
-    if (strcmp(words[i], expected[i]) != 0) {
-      fprintf(stderr, "Expected \"%s\" got \"%s\"\n", words[i], expected[i]);
+    /* Call parse function to be tested */
+    int res = parse_tokens(test_str, words, tokens);
+    if (res != num_words) {
+        fprintf(stderr, "Wrong number of tokens\n");
     }
-  }
 
-  /* Verify tokens */
-  for (int i = 0; i < num_words; i++) {
-    if (tokens[i] != expected_tokens[i]) {
-      fprintf(stderr, "Token %d doesn't match\n", i);
+    /* Check for correct split of words */
+    for (int i = 0; i < num_words; i++) {
+        if (strcmp(words[i], expected[i]) != 0) {
+            fprintf(stderr, "Expected \"%s\" got \"%s\"\n", words[i], expected[i]);
+        }
     }
-  }
+
+    /* Verify tokens */
+    for (int i = 0; i < num_words; i++) {
+        if (tokens[i] != expected_tokens[i]) {
+            fprintf(stderr, "Token %d doesn't match\n", i);
+        }
+    }
 }
 
 /* Test quote handling, whitespace, and redirect handling */
 void parse_commandWithRedirectAndQuotes_splitByDelimiters() {
-  char *test_str = "grep \"\\\"\" <logfile.txt";
+    char *test_str = "grep \"\\\"\" <logfile.txt";
 
-  /* Expected results */
-  int num_words = 4;
-  char *expected[] = {"grep", "\"\\\"\"", "<", "logfile.txt"};
-  token_type expected_tokens[] = {WORD, WORD, IN_REDIR, WORD};
+    /* Expected results */
+    int num_words = 4;
+    char *expected[] = {"grep", "\"\\\"\"", "<", "logfile.txt"};
+    token_type expected_tokens[] = {WORD, WORD, IN_REDIR, WORD};
 
-  char **words = malloc(KiB(1));
-  token_type *tokens = malloc(KiB(1) * sizeof(token_type));
+    char **words = malloc(KiB(1));
+    token_type *tokens = malloc(KiB(1) * sizeof(token_type));
 
-  /* Call parse function to be tested */
-  int res = parse_tokens(test_str, words, tokens);
-  if (res != num_words) {
-    fprintf(stderr, "Wrong number of tokens\n");
-  }
-
-  /* Check for correct split of words */
-  for (int i = 0; i < num_words; i++) {
-    if (strcmp(words[i], expected[i]) != 0) {
-      fprintf(stderr, "Expected \"%s\" got \"%s\"\n", words[i], expected[i]);
+    /* Call parse function to be tested */
+    int res = parse_tokens(test_str, words, tokens);
+    if (res != num_words) {
+        fprintf(stderr, "Wrong number of tokens\n");
     }
-  }
 
-  /* Verify tokens */
-  for (int i = 0; i < num_words; i++) {
-    if (tokens[i] != expected_tokens[i]) {
-      fprintf(stderr, "Token %d doesn't match\n", i);
+    /* Check for correct split of words */
+    for (int i = 0; i < num_words; i++) {
+        if (strcmp(words[i], expected[i]) != 0) {
+            fprintf(stderr, "Expected \"%s\" got \"%s\"\n", words[i], expected[i]);
+        }
     }
-  }
+
+    /* Verify tokens */
+    for (int i = 0; i < num_words; i++) {
+        if (tokens[i] != expected_tokens[i]) {
+            fprintf(stderr, "Token %d doesn't match\n", i);
+        }
+    }
 }
 
 int main() {
-  parse_basicCommand_splitByWhitespace();
-  parse_commandWithRedirectAndQuotes_splitByDelimiters();
-  return 0;
+    parse_basicCommand_splitByWhitespace();
+    parse_commandWithRedirectAndQuotes_splitByDelimiters();
+    return 0;
 }
 
--- src/shell/tokenizer.c
+++ src/shell/tokenizer.c
@@ -5,108 +5,108 @@
 #include <string.h>
 
 int parse_tokens(char *line, char **words, token_type *tokens) {
-  size_t len = strlen(line);
-  token_type *char_tokens = tokenize(line);
-  char *curr_word = malloc(KiB(1));
-  int idx, curr_word_idx = 0;
+    size_t len = strlen(line);
+    token_type *char_tokens = tokenize(line);
+    char *curr_word = malloc(KiB(1));
+    int idx, curr_word_idx = 0;
 
-  for (int i = 0; i < len; i++) {
-    if (!is_delimiter(char_tokens[i])) {
-      /* Save current character */
-      curr_word[curr_word_idx] = line[i];
-      curr_word_idx++;
+    for (int i = 0; i < len; i++) {
+        if (!is_delimiter(char_tokens[i])) {
+            /* Save current character */
+            curr_word[curr_word_idx] = line[i];
+            curr_word_idx++;
+        }
+        /* Handle whitespace, pipes, and redirects */
+        else {
+            /* Add finished word */
+            if (curr_word_idx > 0) {
+                words[idx] = curr_word;
+                tokens[idx] = WORD;
+                idx++;
+                curr_word_idx = 0;
+                curr_word = malloc(KiB(1));
+            }
+            /* Add pipe or redirect operator */
+            if (char_tokens[i] != WHITE) {
+                words[idx] = malloc(sizeof(char));
+                words[idx][0] = line[i];
+                tokens[idx] = char_tokens[i];
+                idx++;
+            }
+        }
     }
-    /* Handle whitespace, pipes, and redirects */
-    else {
-      /* Add finished word */
-      if (curr_word_idx > 0) {
+
+    /* Add last word */
+    if (curr_word_idx > 0) {
         words[idx] = curr_word;
         tokens[idx] = WORD;
         idx++;
         curr_word_idx = 0;
-        curr_word = malloc(KiB(1));
-      }
-      /* Add pipe or redirect operator */
-      if (char_tokens[i] != WHITE) {
-        words[idx] = malloc(sizeof(char));
-        words[idx][0] = line[i];
-        tokens[idx] = char_tokens[i];
-        idx++;
-      }
     }
-  }
 
-  /* Add last word */
-  if (curr_word_idx > 0) {
-    words[idx] = curr_word;
-    tokens[idx] = WORD;
-    idx++;
-    curr_word_idx = 0;
-  }
-
-  return idx;
+    return idx;
 }
 
 token_type *tokenize(char *line) {
-  size_t len = strlen(line);
-  static token_type *tokens;
-  tokens = malloc(len * sizeof(token_type));
-  bool in_string = false;
+    size_t len = strlen(line);
+    static token_type *tokens;
+    tokens = malloc(len * sizeof(token_type));
+    bool in_string = false;
 
-  /* Parse line and label each character */
-  for (int i = 0; i < len; i++) {
-    char curr_char = line[i];
-    token_type curr_token = get_token(in_string, curr_char);
+    /* Parse line and label each character */
+    for (int i = 0; i < len; i++) {
+        char curr_char = line[i];
+        token_type curr_token = get_token(in_string, curr_char);
 
-    /* Handle tokens outside of quotes */
-    if (!in_string) {
-      if (curr_token == QUOTE) {
-        in_string = true;
-      }
-      tokens[i] = curr_token;
+        /* Handle tokens outside of quotes */
+        if (!in_string) {
+            if (curr_token == QUOTE) {
+                in_string = true;
+            }
+            tokens[i] = curr_token;
+        }
+        /* Handle tokens within quotes */
+        else {
+            if (curr_token == QUOTE) {
+                in_string = false;
+            }
+            /* Parse two characters when handling backslash */
+            else if (curr_char == '\\') {
+                tokens[i] = curr_token;
+                i++;
+                curr_char = line[i];
+                curr_token = WORD;
+            }
+            tokens[i] = curr_token;
+        }
     }
-    /* Handle tokens within quotes */
-    else {
-      if (curr_token == QUOTE) {
-        in_string = false;
-      }
-      /* Parse two characters when handling backslash */
-      else if (curr_char == '\\') {
-        tokens[i] = curr_token;
-        i++;
-        curr_char = line[i];
-        curr_token = WORD;
-      }
-      tokens[i] = curr_token;
-    }
-  }
 
-  return tokens;
+    return tokens;
 }
 
 token_type get_token(bool in_string, char curr_char) {
-  if (curr_char == '\"') {
-    return QUOTE;
-  }
-  /* If inside quotes, must be character */
-  else if (in_string) {
-    return WORD;
-  }
-  else {
-    switch (curr_char) {
-      case '|': return PIPE;
-      case '<': return IN_REDIR;
-      case '>': return OUT_REDIR;
-      case ' ': return WHITE;
-      case '\t': return WHITE;
-      /* WORD represents a character within a word */
-      default: return WORD;
+    if (curr_char == '\"') {
+        return QUOTE;
     }
-  }
+    /* If inside quotes, must be character */
+    else if (in_string) {
+        return WORD;
+    }
+    else {
+        switch (curr_char) {
+            case '|': return PIPE;
+            case '<': return IN_REDIR;
+            case '>': return OUT_REDIR;
+            case ' ': return WHITE;
+            case '\t': return WHITE;
+                       /* WORD represents a character within a word */
+            default: return WORD;
+        }
+    }
 }
 
 bool is_delimiter(token_type token) {
-  return token == PIPE || token == IN_REDIR ||
-    token == OUT_REDIR || token == WHITE;
+    return token == PIPE || token == IN_REDIR ||
+        token == OUT_REDIR || token == WHITE;
 }
 
