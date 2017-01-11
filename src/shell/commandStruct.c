c2b514e0fcf1e2031cc631a12fa1bdae2c9e2380
John Li <johnzli1995@gmail.com>
2017-01-10 23:10:14 -0800

    Implemented Command struct with no memory leaks.
    Command struct has been implemented with no memory leaks. Tests
    will be written now and then I will begin to work on the parsing
    for CommandLinkedList and pipes, as well as tests.

--- src/shell/commandStruct.c
+++ src/shell/commandStruct.c
@@ -1,5 +1,9 @@
 #include "commandStruct.h"
+
 #include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
+#include <string.h>
 
 /* * * * * * * * * * * * * * * * * * * * * * * * *
  *         Redirection struct functions          *
@@ -7,7 +11,7 @@
 
 /* Non dynamic constructor. */
 Redirection Redirection_new(const token_type redirect_type,
-                           char* redirect_location) {
+                            char *redirect_location) {
   Redirection new_redirect;
 
   new_redirect.redirect_type = redirect_type;
@@ -18,27 +22,48 @@ Redirection Redirection_new(const token_type redirect_type,
 }
 
 /* Dynamic constructor */
-Redirection* Redirection_new_pointer(const token_type redirect_type,
-                                     char* redirect_location) {
-  Redirection* new_redirect = malloc(sizeof(Redirection));
+Redirection *Redirection_new_pointer(const token_type redirect_type,
+                                     char *redirect_location) {
+  Redirection *new_redirect = malloc(sizeof(Redirection));
+  if (new_redirect) {
+    new_redirect->redirect_type = redirect_type;
 
-  new_redirect->redirect_type = redirect_type;
+    char *file_name = malloc((strlen(redirect_location) + 1) * sizeof(char));
+    if (file_name) {
+      strcpy(file_name, redirect_location);
 
-  /* TODO: Make this deep later. */
-  new_redirect->redirect_location = redirect_location;
-  return new_redirect;
+      new_redirect->redirect_location = file_name;
+      return new_redirect;
+    }
+    else {
+      fprintf(stderr, "Malloc Redirection failed");
+      free(file_name);
+      free(new_redirect);
+      return NULL;
+    }
+  }
+  else {
+    fprintf(stderr, "Malloc Redirection failed");
+    free(new_redirect);
+    return NULL;
+  }
+
 }
 
 /* Non Dynamic destructor. Just frees the array */
-void Redirection_free(Redirection* redirect_pointer) {
+void Redirection_free(Redirection *redirect_pointer) {
   /* TODO: Free the array */
+  free(redirect_pointer->redirect_location);
 }
 
 
-
 /* Dynamic destructor. Frees both struct and array. */
-void Redirection_free_pointer(Redirection* redirect_pointer) {
+void Redirection_free_pointer(Redirection *redirect_pointer) {
   /* TODO: Free the array before freeing the struct. */
+  if (redirect_pointer) {
+    free(redirect_pointer->redirect_location);
+  }
+
   free(redirect_pointer);
 }
 
@@ -52,32 +77,141 @@ void Redirection_free_pointer(Redirection* redirect_pointer) {
 /* Parsing will be done in these constructors. */
 
 /* Non dynamic constructor */
-Command Command_new(char** command_line, token_type** tokens) {
+Command Command_new(char **command_line, token_type *tokens, int size_of_array) {
   Command new_command;
 
   return new_command;
 }
 
-/* Dynamic constructor */
-Command* Command_new_pointer(char** command_line, token_type** tokens) {
+/* Dynamic constructor. Will only parse things before a pipe.
+ * Thus, assume command_line will not contain a pipe.
+ */
+Command* Command_new_pointer(char **command_line, token_type *tokens, int size_of_array) {
+  /* To avoid a ton of if/else indentations,
+   * I will do a few mallocs here first.
+   */
   Command* new_command = malloc(sizeof(Command));
+  char **args = malloc((size_of_array + 1) * sizeof(char *));
+  char *command_word = NULL;
+  char **new_args = NULL;
+  int filtered_idx = 0;
+  int error_idx;
+  int iter;
+
+  /* Handler for freeing everything before returning NULL */
+  if (!args || !new_command) {
+    error_exit:
+      fprintf(stderr, "Command Malloc Error or Tokenize parse error\n");
+      for (error_idx = 0; error_idx < filtered_idx; error_idx++) {
+        free(args[error_idx]);
+      }
+      free(new_args);
+      free(command_word);
+      free(args);
+      free(new_command);
+
+      return NULL;
+  }
+
+  /* All these are set to null so freeing would not be a problem */
+  new_command->next_command = NULL;
+  new_command->prev_command = NULL;
+  new_command->stdin_redirect = NULL;
+  new_command->stdout_redirect = NULL;
+  new_command->stderr_redirect = NULL;
+
+  /* Go through command, and parse out redirects. */
+  for (iter = 0; iter < size_of_array; iter++) {
+    switch (tokens[iter]) {
+
+      case WORD: ; /* Empty statement, 
+                    * because you can't initialize variable immediately after a label
+                    */
+        command_word = malloc((strlen(command_line[iter]) + 1) * sizeof(char));
+        if (command_word) {
+          strcpy(command_word, command_line[iter]);
+          args[filtered_idx] = command_word;
+          filtered_idx++;
+          break;
+        }
+        else {
+          goto error_exit;
+        }
+
+     /* Word after a redirect is the target. So we will increment 
+      * iter additionally here
+      */
+      case IN_REDIR:
+      case OUT_REDIR: ;
+        token_type redirect_type = tokens[iter];
+        iter++;
+        Redirection *new_redirect = Redirection_new_pointer(redirect_type, command_line[iter]);
+
+        if (new_redirect) {
+          if (new_redirect->redirect_type == IN_REDIR) {
+            new_command->stdin_redirect = new_redirect;
+          }
+          else if (new_redirect->redirect_type == OUT_REDIR) {
+            new_command->stdout_redirect = new_redirect;
+          }
+          else {
+            new_command->stderr_redirect = new_redirect;
+          }
+
+          break;
+        }
+        else {
+          goto error_exit;
+        }
+
+      default:
+        /* Must be a faulty token. */
+        goto error_exit;
+    }
+  }
+
+  new_command->num_tokens = filtered_idx;
+
+  /* Reallocate args to have the appropriate size) */
+  new_args = realloc(args, (filtered_idx + 1) * sizeof(char*));
+  args = NULL;
+  if (new_args) {
+    new_args[filtered_idx] = NULL; /* Protocol to end array with NULL */
+    new_command->args = new_args;
+  }
+  else {
+    goto error_exit;
+  }
 
   return new_command;
 }
 
 /* Non dynamic destructor */
-void Command_free(Command* command_pointer) {
+void Command_free(Command *command_pointer) {
 
 }
 
 
 
 /* Dynamic destructor */
-void Command_free_pointer(Command* command_pointer) {
+void Command_free_pointer(Command *command_pointer) {
+  int idx;
 
+  Redirection_free_pointer(command_pointer->stdin_redirect);
+  Redirection_free_pointer(command_pointer->stdout_redirect);
+  Redirection_free_pointer(command_pointer->stderr_redirect);
+
+  /* Accounting for the null at the end of the args */
+  for (idx = 0; idx < command_pointer->num_tokens + 1; idx++) {
+    free(command_pointer->args[idx]);
+  }
+
+  free(command_pointer->args);
+  free(command_pointer);
 }
 
-CommandLinkedList CommandLinkedList_new(Command* first_command) {
+
+CommandLinkedList CommandLinkedList_new(Command *first_command) {
   CommandLinkedList command_LL;
 
   /* No need to deep copy here. */
@@ -87,8 +221,8 @@ CommandLinkedList CommandLinkedList_new(Command* first_command) {
   return command_LL;
 }
 
-CommandLinkedList* CommandLinkedList_new_pointer(Command* first_command) {
-  CommandLinkedList* command_LL = malloc(sizeof(CommandLinkedList));
+CommandLinkedList *CommandLinkedList_new_pointer(Command *first_command) {
+  CommandLinkedList *command_LL = malloc(sizeof(CommandLinkedList));
 
   /* No need to deep copy here. */
   command_LL->first_command = first_command;
--- src/shell/commandStruct.h
+++ src/shell/commandStruct.h
@@ -1,6 +1,7 @@
 #ifndef COMMAND_STRUCT_H_
 #define COMMAND_STRUCT_H_
 
+#include "tokenizer.h"
 /* * * * * * * * * * * * * * * * * * * * * * * * *
  *               Redirection struct              *
  * * * * * * * * * * * * * * * * * * * * * * * * */
@@ -10,22 +11,22 @@
  */
 typedef struct _Redirection {
   token_type redirect_type;
-  char* redirect_location;
+  char *redirect_location;
 } Redirection;
 
 /* Non dynamic constructor. */
 Redirection Redirection_new(const token_type redirect_type,
-                           char* redirect_location);
+                            char *redirect_location);
 
 /* Dynamic constructor */
 Redirection* Redirection_new_pointer(const token_type redirect_type,
-                                   char* redirect_location);
+                                     char *redirect_location);
 
 /* Non dynamic destructor */
-void Redirection_free(Redirection* redirect_pointer);
+void Redirection_free(Redirection *redirect_pointer);
 
 /* Dynamic destructor */
-void Redirection_free_pointer(Redirection* redirect_pointer);
+void Redirection_free_pointer(Redirection *redirect_pointer);
 
 
 
@@ -41,29 +42,31 @@ void Redirection_free_pointer(Redirection* redirect_pointer);
 typedef struct _Command Command;
 typedef struct _Command {
   /* Keeps track of all redirections in the command (input, output, error) */
-  Redirection** redirects;
+  Redirection *stdin_redirect;
+  Redirection *stdout_redirect;
+  Redirection *stderr_redirect;
 
   /* Number of tokens for a particular command */
   int num_tokens;
 
   /* Array of arguments from command line. */
-  char** args;
+  char **args;
 
-  Command* next_command;
-  Command* prev_command;
+  Command *next_command;
+  Command *prev_command;
 } Command;
 
 /* Non dynamic constructor */
-Command Command_new(char** command_line, token_type** tokens);
+Command Command_new(char **command_line, token_type *tokens, int size_of_array);
 
 /* Dynamic constructor */
-Command* Command_new_pointer(char** command_line, token_type** tokens);
+Command *Command_new_pointer(char **command_line, token_type *tokens, int size_of_array);
 
 /* Non dynamic destructor */
-void Command_free(Command* command_pointer);
+void Command_free(Command *command_pointer);
 
 /* Dynamic destructor */
-void Command_free_pointer(Command* command_pointer);
+void Command_free_pointer(Command *command_pointer);
 
 
 
@@ -73,22 +76,22 @@ void Command_free_pointer(Command* command_pointer);
  * previous commands.
  */
 typedef struct _CommandLinkedList {
-  Command* first_command;
+  Command *first_command;
 
   /* Size of the linked list. */
   int linked_list_size;
 } CommandLinkedList;
 
 /* Non dynamic constructor */
-CommandLinkedList CommandLinkedList_new(Command* first_command);
+CommandLinkedList CommandLinkedList_new(Command *first_command);
 
 /* Dynamic constructor. */
-CommandLinkedList* CommandLinkedList_new_pointer(Command* first_command);
+CommandLinkedList *CommandLinkedList_new_pointer(Command *first_command);
 
 /* Non dynamic destructor */
-void CommandLinkedList_free(CommandLinkedList* command_LL_pointer);
+void CommandLinkedList_free(CommandLinkedList *command_LL_pointer);
 
 /* Dynamic destructor */
-void CommandLinkedList_free_pointer(CommandLinkedList* command_LL_pointer);
+void CommandLinkedList_free_pointer(CommandLinkedList *command_LL_pointer);
 
 #endif /* COMMAND_STRUCT_H_ */
