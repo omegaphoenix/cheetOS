f61c1f40c045c40c575b721061b6c16f6f118ee0
Christina Lin <cylin@caltech.edu>
2017-01-10 17:09:55 -0800

    Fix indents, naming, comment style

--- src/shell/builtin.c
+++ src/shell/builtin.c
@@ -4,23 +4,23 @@
 #include <stdlib.h>
 #include <unistd.h>
 
-/* 
-* Internal cd command.
-*/
+/*
+ * Internal cd command.
+ */
 int in_cd(char **args) {
-  /* If no argument is specified, change to home directory */
-  if (args[1] == NULL) {
-    return chdir(getenv("HOME"));
-  }
-  else {
-    return chdir(args[1]);
-  }
-  return 1;
+    /* If no argument is specified, change to home directory */
+    if (args[1] == NULL) {
+        return chdir(getenv("HOME"));
+    }
+    else {
+        return chdir(args[1]);
+    }
+    return 1;
 }
 
-/* 
-* Internal exit command.
-*/
+/*
+ * Internal exit command.
+ */
 int in_exit(char **args) {
-  exit(0);
+    exit(0);
 }
--- src/shell/builtin.h
+++ src/shell/builtin.h
@@ -1,15 +1,20 @@
-/* 
-* Internal cd command.
-* 
-* Changes current working directory of the shell. The shell changes to the
-* directory specified by the first and only argument. If no argument is
-* specified, the shell changes to user's home directory.
-*/
+#ifndef BUILTIN_H
+#define BUILTIN_H
+
+/*
+ * Internal cd command.
+ * 
+ * Changes current working directory of the shell. The shell changes to the
+ * directory specified by the first and only argument. If no argument is
+ * specified, the shell changes to user's home directory.
+ */
 int in_cd(char **args);
 
-/* 
-* Internal exit command.
-* 
-* Terminates the shell.
-*/
+/*
+ * Internal exit command.
+ * 
+ * Terminates the shell.
+ */
 int in_exit(char **args);
+
+#endif /* BUILTIN_H */
