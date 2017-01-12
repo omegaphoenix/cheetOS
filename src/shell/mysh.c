7b6f56c1afcebea1e67f77ba0dc8f4038ef0af21
Christina Lin <cylin@caltech.edu>
2017-01-11 17:09:27 -0800

    Implement prompt and internal commands in mainloop
    The prompt prints "username:cwd> " before reading in the user input into
    input_str. The input string is tokenized, returning an array of words. The loop
    checks if the first word is an internal command ("cd" or "exit"), and if it is,
    the corresponding internal command is run.
    
    For the actual, final version of the mainloop, the tokens will (probably) be
    converted to Command structs before executing the internal or external
    commands. TODO statements have been added regarding these changes.

--- src/shell/mysh.c
+++ src/shell/mysh.c
@@ -1,14 +1,51 @@
+#include "builtin.h"
 #include "tokenizer.h"
 
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
 
 int main() {
-  char str[KiB(1)];
+	char *cwd;
+	char *username;
+    char cwd_buffer[KiB(1) + 1];
 
-  printf("Enter a string:");
-  fgets(str, KiB(1), stdin);
+    char input_str[KiB(1)];
 
-  printf("\nYou entered: %s ", str);
-  printf("\n");
-  return 0;
+	while (1) {
+		/* Prompt with username and current working directory */
+		cwd = getcwd(cwd_buffer, KiB(1) + 1);
+		username = getlogin();
+
+		printf("%s:%s> ", username, cwd);
+		fgets(input_str, KiB(1), stdin);
+
+		/* Remove newline (and any following chars) at the end of input_str*/
+		input_str[strcspn(input_str, "\n")] = 0;
+
+		/* Tokenize input command */
+		char **words = malloc(KiB(1));
+		token_type *tokens = malloc(KiB(1) * sizeof(token_type));
+		int num_tokens = parse_tokens(input_str, words, tokens);
+
+		/* TODO: Create Command struct from tokens */
+		if (num_tokens) {}; // delete this
+
+		/* TODO: improve this by looping through array of internal cmds */
+		/* If internal command, execute in current process */
+		if (strcmp(words[0], "cd") == 0) {
+			in_cd(words);
+		}
+		else if (strcmp(words[0], "exit") == 0) {
+			in_exit(words);
+		}
+
+		/* TODO: Else, fork a child process and execute */
+		else {
+			printf("You entered: %s", input_str);
+			printf("\n");
+		}
+	}
+    return 0;
 }
