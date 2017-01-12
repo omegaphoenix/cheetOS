2f3a04bc0fa2a7ab989ed9f7fb6e13566af29727
Justin Leong <justinleong16@gmail.com>
2017-01-12 13:33:02 -0800

    Fix compiler warning
    The variable finished_pid is only used in the parent process so it
    is unused in the child fork.

--- src/shell/extCmd.c
+++ src/shell/extCmd.c
@@ -7,7 +7,7 @@
 #include <unistd.h>
 
 int execute_cmd(char **args) {
-    pid_t child_pid, finished_pid;
+    pid_t child_pid;
     int status;
 
     /* Fork off a child process to execute program */
@@ -23,9 +23,9 @@ int execute_cmd(char **args) {
     }
     else {
         /* Wait for child process to terminate */
-        finished_pid = wait(&status);
+        pid_t finished_pid = wait(&status);
         while (finished_pid != child_pid) {
-            pid_t finished_pid = wait(&status);
+            finished_pid = wait(&status);
         }
 
         return status;
