b9c677759f9fc17ea596345304456841c0c4f128
Christina Lin <cylin@caltech.edu>
2017-01-13 00:34:52 -0800

    Implement rediction for stdin and stdout
    After forking a child process, execute_cmd() creates a new file descriptor
    for the specified file, copies this file descriptor to stdin or stdout file
    descriptor, and closes the unused file descriptors.

--- src/shell/extCmd.c
+++ src/shell/extCmd.c
@@ -1,5 +1,6 @@
 #include "extCmd.h"
 
+#include <fcntl.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -24,6 +25,36 @@ int execute_cmd(Command *cmd) {
     /* Child process */
     if (pid == 0) {
         /* Execute command with child process */
+
+        /* Handle redirection, if necessary */
+
+        mode_t mode = S_IRUSR | S_IWUSR | S_IRGRP | S_IROTH;
+        /* mode gives read and write permissions to file owner,
+         * and read permissions to the group owner and other users.
+         */
+
+        if (cmd->stdin_redirect != NULL) {
+            int stdin_filedes = open(cmd->stdin_redirect->redirect_location,
+                                     O_RDONLY); /* read only */
+            dup2(stdin_filedes, STDIN_FILENO);
+            close(stdin_filedes);
+        }
+        if (cmd->stdout_redirect != NULL) {
+            /* write only; create file if it doesn't exist */  
+            int stdout_filedes = open(cmd->stdout_redirect->redirect_location,
+                                      O_WRONLY | O_CREAT | O_TRUNC, mode);
+            
+            dup2(stdout_filedes, STDOUT_FILENO);
+            close(stdout_filedes);
+        }
+        if (cmd->stderr_redirect != NULL) {
+            /* write only; create file if it doesn't exist */
+            int stderr_filedes = open(cmd->stderr_redirect->redirect_location,
+                                     O_WRONLY | O_CREAT, mode);
+            
+            dup2(stderr_filedes, STDERR_FILENO);
+            close(stderr_filedes);
+        }
 
         /* This process should never return if successful */
         execvp(argv[0], argv);
