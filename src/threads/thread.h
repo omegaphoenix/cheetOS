21e1c06348f7fd522a9657d021ad6f60d1926388
Justin Leong <justinleong16@gmail.com>
2017-02-03 16:38:21 -0800

    Add B7 to finish design doc

--- src/threads/thread.h
+++ src/threads/thread.h
@@ -114,7 +114,7 @@ struct thread {
 
     /*! Shared between thread.c and synch.c. */
     /**@{*/
-    struct list_elem lock_elem;         /*!< List element. */
+    struct list_elem lock_elem;         /*!< List element for lock's blocked_threads. */
     /**@}*/
 
 #ifdef USERPROG
