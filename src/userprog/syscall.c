8f7789934fc21589b4012a66fb53f7f72be56cd7
Justin Leong <justinleong16@gmail.com>
2017-02-09 19:54:35 -0800

    Print proccess's name and exit code before exit

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -5,6 +5,7 @@
 #include "threads/thread.h"
 
 static void syscall_handler(struct intr_frame *);
+void sys_exit(int status);
 
 void syscall_init(void) {
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
@@ -16,5 +17,6 @@ static void syscall_handler(struct intr_frame *f UNUSED) {
 }
 
 void sys_exit(int status) {
+    printf("%s:exit(%d)\n", thread_current()->name, status);
     thread_exit();
 }
