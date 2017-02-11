0c71fac374522956c5866e8ba17d3611e68f04eb
Christina Lin <cylin@caltech.edu>
2017-02-11 00:33:18 -0800

    Begin implementation of user memory access
    is_valid_addr() will check if the given address is valid

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -1,13 +1,19 @@
 #include "userprog/syscall.h"
+#include <stdbool.h>
 #include <stdio.h>
 #include <syscall-nr.h>
 #include "devices/shutdown.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
 
 static void syscall_handler(struct intr_frame *);
 void sys_halt(void);
 void sys_exit(int status);
+
+static int get_user(const uint8_t *uaddr) UNUSED;
+static bool put_user (uint8_t *udst, uint8_t byte) UNUSED;
+static bool is_valid_addr(const void *vaddr) UNUSED;
 
 void syscall_init(void) {
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
@@ -28,4 +34,29 @@ void sys_halt(void) {
 void sys_exit(int status) {
     printf("%s:exit(%d)\n", thread_current()->name, status);
     thread_exit();
+}
+
+static bool is_valid_addr(const void *addr) {
+    return addr != NULL && is_user_vaddr(addr); 
+    // lookup_page(active_pd(), addr, false);
+}
+
+/*! Reads a byte at user virtual address UADDR.
+    UADDR must be below PHYS_BASE.
+    Returns the byte value if successful, -1 if a segfault occurred. */
+static int get_user(const uint8_t *uaddr) {
+    int result;
+    asm ("movl $1f, %0; movzbl %1, %0; 1:"
+         : "=&a" (result) : "m" (*uaddr));
+    return result;
+}
+
+/*! Writes BYTE to user address UDST.
+    UDST must be below PHYS_BASE.
+    Returns true if successful, false if a segfault occurred. */
+static bool put_user (uint8_t *udst, uint8_t byte) {
+    int error_code;
+    asm ("movl $1f, %0; movb %b2, %1; 1:"
+         : "=&a" (error_code), "=m" (*udst) : "q" (byte));
+    return error_code != -1;
 }
