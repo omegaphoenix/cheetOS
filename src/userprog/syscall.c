51e713cc1fcfd3d2d814220793f5b803d15797b9
Justin Leong <justinleong16@gmail.com>
2017-02-11 02:43:00 -0800

    Implement sys_write for writing to fd 1

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -8,6 +8,7 @@
 static void syscall_handler(struct intr_frame *);
 void sys_halt(void);
 void sys_exit(int status);
+int sys_write(int fd, const void *buffer, unsigned size);
 
 void syscall_init(void) {
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
@@ -28,4 +29,24 @@ void sys_halt(void) {
 void sys_exit(int status) {
     printf("%s:exit(%d)\n", thread_current()->name, status);
     thread_exit();
+}
+
+/*! Writes size bytes from buffer to the open file fd. Returns the number of
+		bytes actually written, which may be less than size if some bytes could
+		not be written.
+		Writing past end-of-file would normally extend the file, but file growth
+		is not implemented by the basic file system. The expected behavior is to
+		write as many bytes as possible up to end-of-file and return the actual
+		number written, or 0 if no bytes could be written at all.
+		Fd 1 writes to the console. Your code to write to the console should write
+		all of buffer in one call to putbuf(), at least as long as size is not
+		bigger than a few hundred bytes. (It is reasonable to break up larger
+		buffers.) Otherwise, lines of text output by different processes may end
+		up interleaved on the console, confusing both human readers and our
+		grading scripts. */
+int sys_write(int fd, const void *buffer, unsigned size) {
+		if (fd == STDIN_FILENO) {
+        putbuf(buffer, size);
+		}
+    return 0;
 }
