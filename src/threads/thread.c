4bf640b813ab6c86b45316fecdca92295dba89e9
Justin Leong <justinleong16@gmail.com>
2017-02-14 01:00:55 -0800

    Add assertion to check fd range

--- src/threads/thread.c
+++ src/threads/thread.c
@@ -530,6 +530,7 @@ int add_open_file(struct thread *cur, struct file *file, int fd) {
 /*! Get file with file descriptor *fd*. */
 struct file *get_fd(struct thread *cur, int fd) {
     int index = fd - CONSOLE_FD;
+    ASSERT(index >= 0 && index < MAX_FD);
     struct file *open_file = cur->open_files[index];
     return open_file;
 }
