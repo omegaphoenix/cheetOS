851b39be466c91febb600791cd3875bcc0cb1f5d
Justin Leong <justinleong16@gmail.com>
2017-02-17 01:14:07 -0800

    Remove double check valid pointer

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -241,9 +241,6 @@ int sys_read(int fd, void *buffer, unsigned size) {
         sys_exit(ERR);
     }
     int bytes_read = 0;
-    if (!valid_read_addr(buffer)) {
-        sys_exit(ERR);
-    }
     /* Pointer to point to current position in buffer */
     char *buff = (char *) buffer;
 
@@ -278,9 +275,6 @@ int sys_write(int fd, const void *buffer, unsigned size) {
         sys_exit(ERR);
     }
     int bytes_written = 0;
-    if (!valid_read_addr(buffer)) {
-        sys_exit(ERR);
-    }
 
     /* Write to console */
     if (fd == STDOUT_FILENO) {
