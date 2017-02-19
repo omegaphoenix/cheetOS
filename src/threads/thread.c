omegaphoenix/cheetOS

commit 63841282141f061e1ceb146f83cf4a72740f9e51

Fix bug in initialization of waited_on
Since it was set to true, we never allowed processes to wait on the
thread again.

src/threads/thread.c
@@ -721,7 +721,7 @@ static void init_thread(struct thread *t, const char *name, int priority) {
     sema_init(&t->exec_load, 0);
     lock_init(&t->filesys_lock);
     t->loaded = false;
-    t->waited_on = true;
+    t->waited_on = false;
     t->num_files = 0;
 
     if (list_empty(&all_list)) {
@@ -829,7 +829,6 @@ void thread_schedule_tail(struct thread *prev) {
         /* Let parent know it is done. */
         sema_up(&prev->wait_sema);
         while (!list_empty(&prev->wait_sema.waiters)) {
-            printf("more sema up\n");
             sema_up(&prev->wait_sema);
         }
         if (prev->parent == NULL) {

src/userprog/process.c
@@ -154,8 +154,7 @@ int process_wait(tid_t child_tid UNUSED) {
          e = list_next(e)) {
         kid = list_entry(e, struct thread, kid_elem);
         if (kid->tid == child_tid && !kid->waited_on) {
-            /* Remove so next time we look for this kid, we return -1. */
-            // list_remove(&kid->kid_elem);
+            /* Next time we look for this kid, we return -1. */
             kid->waited_on = true;
             break;
         }

commit 7ca0df1d96de140b24f4b323599f3db5b94ef01b

Remove while loop that causes syn-read to hang
Fix bug introduced in commit 63841282141f061e1ceb146f83cf4a72740f9e51.

src/threads/thread.c
@@ -828,9 +828,6 @@ void thread_schedule_tail(struct thread *prev) {
         ASSERT(prev != cur);
         /* Let parent know it is done. */
         sema_up(&prev->wait_sema);
-        while (!list_empty(&prev->wait_sema.waiters)) {
-            sema_up(&prev->wait_sema);
-        }
         if (prev->parent == NULL) {
             /* Don't need to wait for parent to kill kid */
             palloc_free_page(prev);