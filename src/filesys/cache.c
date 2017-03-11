7229390fcb2bb72aa576cacddd562aa297d9e1aa
Justin Leong <justinleong16@gmail.com>
2017-03-11 13:14:11 -0800

    Clean up cache implementation

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -1,134 +1,158 @@
+#include "cache.h"
 #include <hash.h>
 #include <stdbool.h>
 #include <string.h>
-
-#include "cache.h"
 #include "devices/block.h"
 #include "filesys/filesys.h"
 #include "threads/synch.h"
 #include "threads/malloc.h"
 
+/* Handle global cache lock. */
+static void acquire_cache_lock(void);
+static void release_cache_lock(void);
+
+/* Functions for hash implementation. */
 static unsigned hash_sector_idx(const struct hash_elem *e, void *aux);
-static bool hash_sector_less(const struct hash_elem *a, const struct hash_elem *b, void *aux);
+static bool hash_sector_less(const struct hash_elem *a,
+        const struct hash_elem *b, void *aux);
 static void hash_cache_free(struct hash_elem *e, void *aux);
 
-/* Will initialize the global hash table. */
+/*! Acquire global cache lock for modifying cache table/hashmap. */
+static void acquire_cache_lock(void) {
+    lock_acquire(&cache_lock);
+}
+
+/*! Release global cache lock for modifying cache table/hashmap. */
+static void release_cache_lock(void) {
+    lock_release(&cache_lock);
+}
+
+/*! Will initialize the global hash table. */
 void cache_table_init(void) {
     hash_init(&cache_sector_table, hash_sector_idx, hash_sector_less, NULL);
     lock_init(&cache_lock);
 }
 
-/* Frees elements after they are removed from table */
+/*! Free elements after they are removed from table. */
 void cache_table_free(void) {
-    // TODO: I'm not too sure when to free this tbh.
     hash_destroy(&cache_sector_table, hash_cache_free);
 }
 
-/* Hashes a cache_sector into the hash table based on its sector idx */
+/*! Hash a cache_sector into the hash table based on its sector idx. */
 static unsigned hash_sector_idx(const struct hash_elem *e, void *aux UNUSED) {
-    struct cache_sector *sector = hash_entry(e, struct cache_sector, cache_sector_elem);
+    struct cache_sector *sector = hash_entry(e, struct cache_sector,
+            cache_sector_elem);
     int hash_idx = hash_int(sector->sector_idx);
 
     return hash_idx;
 }
 
-/* Compares the sector indices for hash element comparisons */
-static bool hash_sector_less(const struct hash_elem *a, const struct hash_elem *b, void *aux UNUSED) {
-    struct cache_sector *first_sector = hash_entry(a, struct cache_sector, cache_sector_elem);
-    struct cache_sector *second_sector = hash_entry(b, struct cache_sector, cache_sector_elem);
+/*! Compares the sector indices for hash element comparisons */
+static bool hash_sector_less(const struct hash_elem *a,
+        const struct hash_elem *b, void *aux UNUSED) {
+    struct cache_sector *first_sector = hash_entry(a, struct cache_sector,
+            cache_sector_elem);
+    struct cache_sector *second_sector = hash_entry(b, struct cache_sector,
+            cache_sector_elem);
 
     return first_sector->sector_idx < second_sector->sector_idx;
 }
 
-/* Frees the hash object from hash_table. */
+/*! Frees the hash object from hash_table. */
 static void hash_cache_free(struct hash_elem *e, void *aux UNUSED) {
     ASSERT(e != NULL);
 
-    struct cache_sector *sector = hash_entry(e, struct cache_sector, cache_sector_elem);
+    struct cache_sector *sector = hash_entry(e, struct cache_sector,
+            cache_sector_elem);
     if (sector != NULL) {
         free(sector);
     }
     sector = NULL;
 }
 
-/* Initialize a new sector_idx, and insert into hash table. */
+/*! Initialize a new sector_idx, and insert into hash table. */
 struct cache_sector *cache_init(block_sector_t sector_idx, void *data) {
     /* First, check that it doesn't exist */
     ASSERT(cache_get(sector_idx) == NULL);
     struct cache_sector *new_sector = malloc(sizeof(struct cache_sector));
 
     new_sector->sector_idx = sector_idx;
-    new_sector->access_level = NOT_ACCESSED;
-    new_sector->is_dirty = false;
+    new_sector->accessed = false;
+    new_sector->dirty = false;
 
     /* Read from memory into buffer */
     block_read(fs_device, sector_idx, data);
 
-    lock_acquire(&cache_lock);
+    acquire_cache_lock();
     hash_insert(&cache_sector_table, &new_sector->cache_sector_elem);
-    lock_release(&cache_lock);
+    release_cache_lock();
     return new_sector;
 }
 
-/* Finds appropriate struct from hash table, and frees it/deletes it from table */
+/*! Finds appropriate struct from hash table, and frees it and deletes it from
+    table */
 void cache_free(block_sector_t sector_idx) {
     struct cache_sector temp_sector;
     temp_sector.sector_idx = sector_idx;
-    lock_acquire(&cache_lock);
-    struct hash_elem *cache_sector_entry = hash_find(&cache_sector_table, &temp_sector.cache_sector_elem);
+    acquire_cache_lock();
+    struct hash_elem *cache_sector_entry = hash_find(&cache_sector_table,
+            &temp_sector.cache_sector_elem);
 
     if (cache_sector_entry) {
-        cache_sector_entry = hash_delete(&cache_sector_table, cache_sector_entry);
+        cache_sector_entry = hash_delete(&cache_sector_table,
+                cache_sector_entry);
 
         ASSERT(cache_sector_entry != NULL);
         ASSERT(cache_get(sector_idx) == NULL);
 
         hash_cache_free(cache_sector_entry, NULL);
     }
-    lock_release(&cache_lock);
+    release_cache_lock();
 }
 
-/* Uses block algorithm to choose the next block to evict */
+/*! Uses block algorithm to choose the next block to evict */
 void cache_evict(void) {
   // TODO
 }
 
-/* Removes a block from the buffer cache. This involves it writing from cache back
-   to memory */
+/*! Removes a block from the buffer cache. This involves it writing from cache
+    back to memory */
 void cache_remove(block_sector_t sector_idx) {
   // TODO
 }
 
-/* Adds a block into buffer cache. Evicts if necessary. Will write from
-   memory to cache */
+/*! Adds a block into buffer cache. Evicts if necessary. Will write from
+    memory to cache */
 void cache_insert(block_sector_t sector_idx, void *data) {
-    lock_acquire(&cache_lock);
+    acquire_cache_lock();
     /* Checks if the cache has already hit maximum capacity */
     if (hash_size(&cache_sector_table) == MAX_BUFFER_SIZE)
         cache_evict();
-    lock_release(&cache_lock);
+    release_cache_lock();
 
     ASSERT(hash_size(&cache_sector_table) < MAX_BUFFER_SIZE);
     cache_init(sector_idx, data);
 }
 
-/* Will retrieve the specific cache from the cache map. */
+/*! Will retrieve the specific cache from the cache map. */
 struct cache_sector *cache_get(block_sector_t sector_idx) {
-    lock_acquire(&cache_lock);
+    acquire_cache_lock();
     struct cache_sector temp_sector;
     struct cache_sector *return_sector = NULL;
     temp_sector.sector_idx = sector_idx;
 
-    struct hash_elem *cache_sector_entry = hash_find(&cache_sector_table, &temp_sector.cache_sector_elem);
+    struct hash_elem *cache_sector_entry = hash_find(&cache_sector_table,
+            &temp_sector.cache_sector_elem);
 
     if (cache_sector_entry) {
-        return_sector = hash_entry(cache_sector_entry, struct cache_sector, cache_sector_elem);
+        return_sector = hash_entry(cache_sector_entry, struct cache_sector,
+                cache_sector_elem);
     }
-    lock_release(&cache_lock);
+    release_cache_lock();
     return return_sector;
 }
 
-/* Will write data to a cache_sector buffer */
+/*! Will write data to a cache_sector buffer */
 void write_to_cache(block_sector_t sector_idx, void *data) {
     struct cache_sector *found_sector = cache_get(sector_idx);
 
@@ -136,11 +160,11 @@ void write_to_cache(block_sector_t sector_idx, void *data) {
     ASSERT(found_sector != NULL);
     memcpy(found_sector->sector, data, BLOCK_SECTOR_SIZE);
 
-    found_sector->is_dirty = true;
+    found_sector->dirty = true;
 }
 
 
-/* Will read from cache and write to memory. Involves freeing. */
+/*! Will read from cache and write to memory. Involves freeing. */
 void read_from_cache(block_sector_t sector_idx, void *data) {
     struct cache_sector *found_sector = cache_get(sector_idx);
 
--- src/filesys/cache.h
+++ src/filesys/cache.h
@@ -7,7 +7,6 @@
 
 #include <hash.h>
 #include <stdbool.h>
-
 #include "devices/block.h"
 #include "threads/synch.h"
 
@@ -15,45 +14,38 @@
 
 struct hash cache_sector_table;
 
-struct lock cache_lock; 
-
-/* Because we've got multiple levels of access, we have an enum
-   type to explain it all */
-enum access_status {
-    NOT_ACCESSED,
-    REGULAR_ACCESS,
-    META_ACCESS
-};
+/* Global lock for protecting cache hash map. */
+struct lock cache_lock;
 
 /* We would like our cache sector to be in a hash table for
    easier lookup. We will use sector index as the key. */
 struct cache_sector {
-    block_sector_t sector_idx;            /*! Sector index on filesys block */
-    enum access_status access_level;           /*! Sector access level */
-    bool is_dirty;                        /*! Boolean if sector is dirty */
-    struct hash_elem cache_sector_elem;   /*! Makes it part of a hash table */
-    uint8_t sector[BLOCK_SECTOR_SIZE];    /*! Each sector is 512 bytes. */
+    block_sector_t sector_idx;          /*! Sector index on filesys block. */
+    bool accessed;                      /*! Sector access level. */
+    bool dirty;                         /*! Boolean if sector is dirty. */
+    struct hash_elem cache_sector_elem; /*! Makes it part of a hash table. */
+    uint8_t sector[BLOCK_SECTOR_SIZE];  /*! Each sector is 512 bytes. */
 };
 
-/* Hash table constructor/destructor */
+/* Hash table constructor/destructor. */
 void cache_table_init(void);
 void cache_table_free(void);
 
-/* cache_sector constructor/destructor. Also removal/insertion into hash table */
+/* cache_sector constructor/destructor. Removal/insertion into hash table. */
 struct cache_sector *cache_init(block_sector_t sector_idx, void *data);
 void cache_free(block_sector_t sector_idx);
 
-/* Might use clock algorithm for this */
+/* Might use clock algorithm for this. */
 void cache_evict(void);
 
-/* Removal and Insertion from buffer cache */
+/* Removal and Insertion from buffer cache. */
 void cache_remove(block_sector_t sector_idx);
 void cache_insert(block_sector_t sector_idx, void *data);
 
-/* Retrieval */
+/* Retrieval. */
 struct cache_sector *cache_get(block_sector_t sector_idx);
 
-/* Writing/Reading to/from disk methods */
+/* Writing/Reading to/from disk methods. */
 void write_to_cache(block_sector_t sector_idx, void *data);
 void read_from_cache(block_sector_t sector_idx, void *data);
 
